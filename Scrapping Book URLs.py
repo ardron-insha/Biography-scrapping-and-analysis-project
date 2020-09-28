9f9f0a8679c964a4eab1c957b448334e0bde935d
Insha <38670082+insha-chaddha@users.noreply.github.com>
2020-09-28 15:32:31 -0400

    Update Scrapping Book URLs.py
    Scraping second page.

--- Scrapping Book URLs.py
+++ Scrapping Book URLs.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Sep 21 14:36:15 2020
 
@@ -13,29 +12,31 @@ import pandas as pd
 
 import requests
 from bs4 import BeautifulSoup
-
-URL = 'https://www.barnesandnoble.com/b/new-releases/_/N-1oyg?Nrpp=40&Ns=P_Publication_Date%7C1&page=1'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.text)
-
-
-items2 = soup.select(".bnBadgeHere")
-
-res = []
-for i in items2:
-    res.append(str(i))
+items = []
+for i in ["1","2"]:
+    URL = 'https://www.barnesandnoble.com/b/new-releases/biography/_/N-1oygZsoc?Nrpp=40&Ns=P_Publication_Date%7C1&page=' + i
+    page = requests.get(URL)
     
-items2=pd.DataFrame({"raw_code": res})
-items2["raw_code"] = items2.raw_code.astype(str)
-items2["raw_code"][0]
-items2["book"] = items2.raw_code.str.split('Title:').str[1]
-items2["book"] = items2.book.str.split(':',1).str[0]
-items2["author"] = items2.raw_code.str.split('Author:').str[1]
-items2["author"] = items2.author.str.split('"',1).str[0]
-items2["author"][0]
-items2["URL"] = items2.raw_code.str.split('data-quickview-url="').str[1]
-items2["URL"] = items2.URL.str.split('"',1).str[0]
-items2["Prefix"] = "https://www.barnesandnoble.com"
-
-items2.to_csv("URLs.csv")
+    soup = BeautifulSoup(page.text)
+    
+    
+    items2 = soup.select(".bnBadgeHere")
+    
+    res = []
+    for i in items2:
+        res.append(str(i))
+        
+    items2=pd.DataFrame({"raw_code": res})
+    items2["raw_code"] = items2.raw_code.astype(str)
+    items2["raw_code"][0]
+    items2["book"] = items2.raw_code.str.split('Title:').str[1]
+    items2["book"] = items2.book.str.split(':',1).str[0]
+    items2["author"] = items2.raw_code.str.split('Author:').str[1]
+    items2["author"] = items2.author.str.split('"',1).str[0]
+    items2["author"][0]
+    items2["URL"] = items2.raw_code.str.split('data-quickview-url="').str[1]
+    items2["URL"] = items2.URL.str.split('"',1).str[0]
+    items2["Prefix"] = "https://www.barnesandnoble.com"
+    items.append(items2)
+    
+items.to_csv(r"C:\Users\ichaddha\Documents\URLs.csv")
